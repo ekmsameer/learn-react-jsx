e5f7008015d6e814fcc90517b706956a9b27fa0c
Mohamed Sameer <mohamed.sameer@playsport.com>
2021-06-20 21:24:39 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () =>{
+    const buttonText={text: "Click me"}
+    const labelText = "Enter Name: "
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelText}    </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor:'blue',color:'white'}} >{buttonText.text}</button>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
